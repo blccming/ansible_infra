dffcb07f8baa1e307a4ff1ac4d83731e063ec485
blccming <115806543+blccming@users.noreply.github.com>
2025-01-26 22:48:14 +0100

    bump geerlingguy.docker to 7.4.4

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-ansible-galaxy install -r requirements.yml -p roles/
+ansible-galaxy install -r requirements.yml -p roles/ --force
 ansible-playbook --vault-password-file=.vault_pass -u bloom -i inventory.ini playbook.yml
