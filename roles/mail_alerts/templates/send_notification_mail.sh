a6b43d54fa0291d84524fb83f5e5f7333d03e196
blccming <115806543+blccming@users.noreply.github.com>
2025-02-02 13:37:15 +0100

    fixed mail script

--- roles/mail_alerts/templates/send_notification_mail.sh
+++ roles/mail_alerts/templates/send_notification_mail.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-subject=$1
-contents=$2
+SUBJECT=$1
+CONTENTS=$2
 
 curl -X POST 'https://api.resend.com/emails' \
   -H 'Authorization: {{ mail_notification_api_key }}' \
   -H 'Content-Type: application/json' \
-  -d '{
-    "from": "🚨 BLCCMING NOTIFICATION <alert@notifier.{{ domain_public }}>",
-    "to": "{{ mail_notification_rcv_address }}",
-    "subject": "$subject",
-    "html": "<p>$contents</p>"
-  }'
+  -d "{
+    \"from\": \"🚨 BLCCMING NOTIFICATION <alert@notifier.{{ domain_public }}>\",
+    \"to\": \"{{ mail_notification_rcv_address }}\",
+    \"subject\": \"$SUBJECT\",
+    \"html\": \"<p>$CONTENTS</p>\"
+  }" > /dev/null 2>&1
