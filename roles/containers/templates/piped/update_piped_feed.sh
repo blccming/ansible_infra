1e3ec9e5542d081211ec6996002663a83e9316b8
blccming <115806543+blccming@users.noreply.github.com>
2025-02-15 19:33:06 +0100

    fix piped feed update script

--- roles/containers/templates/piped/update_piped_feed.sh
+++ roles/containers/templates/piped/update_piped_feed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-subscriptions=$(docker exec -i postgres psql -U piped -d piped -qtAX -c 'select id from public.pubsub;')
+subscriptions=$(docker exec -i piped_postgres psql -U piped -d piped -qtAX -c 'select id from public.pubsub;')
 
 while IFS= read -r line; do
-        curl -k "https://pipedapi.lab.zykla.me/channel/$line" > /dev/null
+        curl -k "https://pipedapi.{{ domain_local }}/channel/$line" > /dev/null
         sleep 1;
 done <<< $subscriptions
