9c6d31d2e0713e0cc3a01b6ab2af73996bdc30f4
blccming <115806543+blccming@users.noreply.github.com>
2025-02-17 18:23:57 +0100

    added missing parenthesis

--- roles/storageconf/templates/zfs_backup.sh
+++ roles/storageconf/templates/zfs_backup.sh
@@ -6,5 +6,5 @@ MAIL_SCRIPT="/home/{{ username }}/bin/send_notification_mail.sh"
 
 syncoid "$SOURCE_POOL" "$TARGET_POOL"
 
-CURRENT_TIME=$(date +"%Y-%m-%dT%H:%M:%S %Z"
+CURRENT_TIME=$(date +"%Y-%m-%dT%H:%M:%S %Z")
 bash "$MAIL_SCRIPT" "💾 {{ hostname }}: Backup performed" "Syncoid completed a backup at <strong>$CURRENT_TIME</strong>."
